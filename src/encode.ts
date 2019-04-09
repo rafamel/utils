26e821f5d4a5990c2153fd1402690ccd80c57ee4
Rafa Mel <melrcm@gmx.net>
2019-04-09 23:19:44 +0200

    feat: implements encode

--- src/encode.ts
+++ src/encode.ts
@@ -6,5 +6,40 @@ export default function encode(
   base: BaseConverter,
   options: IOptions
 ): string {
-  return '';
+  const payload: string[] = [];
+  return (
+    trunk('', str, payload, options) +
+    (payload.length
+      ? options.separator + base.encode(Buffer.from(payload.join('|')))
+      : '')
+  );
+}
+
+export function trunk(
+  str: string,
+  next: string,
+  payload: string[],
+  options: IOptions
+): string {
+  if (!next) return str;
+
+  const index = next.search(options.target);
+  if (index === -1) return str + next;
+
+  const a = next.slice(0, index);
+  const b = next.slice(index, index + 1);
+  const c = next.slice(index + 1);
+
+  str += a;
+  const replaced = options.map(b);
+  payload.push(
+    `${b}${str.length}${
+      replaced.length
+        ? replaced.length === 1
+          ? '.'
+          : '.' + replaced.length
+        : ''
+    }`
+  );
+  return trunk(str + replaced, c, payload, options);
 }
