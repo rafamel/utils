37cec10c1485d77656e5e0c3ace126f922b0cbb7
Rafa Mel <melrcm@gmx.net>
2019-04-14 14:07:51 +0200

    test: adds tests for safePairs

---
+++ test/safe-pairs.test.ts
@@ -0,0 +1,53 @@
+import safePairs from '~/safe-pairs';
+
+const a = { foo: 'foo', bar: 'bar', baz: 'baz' };
+const b = { foo: 'baz', bar: 'bar', baz: 'foo', foobar: 'foobaz' };
+const c = { foo: 'baz', bar: 'bar', foobar: 'foobaz' };
+
+describe(`defaults`, () => {
+  describe(`!fail`, () => {
+    test(`true`, () => {
+      expect(safePairs(a, b)).toBe(true);
+      expect(safePairs(a, b, {})).toBe(true);
+    });
+    test(`false`, () => {
+      expect(safePairs(a, c)).toBe(false);
+      expect(safePairs(a, c, {})).toBe(false);
+    });
+  });
+  describe(`fail`, () => {
+    test(`succeeds`, () => {
+      expect(safePairs(a, b, { fail: true })).toBe(true);
+    });
+    test(`fails`, () => {
+      expect(() => safePairs(a, c, { fail: true })).toThrowError();
+    });
+  });
+});
+describe(`bidirectional`, () => {
+  describe(`!fail`, () => {
+    test(`true`, () => {
+      expect(
+        safePairs({ ...a, foobar: 'foobar' }, b, { bidirectional: true })
+      ).toBe(true);
+    });
+    test(`false`, () => {
+      expect(safePairs(a, b, { bidirectional: true })).toBe(false);
+    });
+  });
+  describe(`fail`, () => {
+    test(`succeeds`, () => {
+      expect(
+        safePairs({ ...a, foobar: 'foobar' }, b, {
+          fail: true,
+          bidirectional: true
+        })
+      ).toBe(true);
+    });
+    test(`fails`, () => {
+      expect(() =>
+        safePairs(a, b, { fail: true, bidirectional: true })
+      ).toThrowError();
+    });
+  });
+});
