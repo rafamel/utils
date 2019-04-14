f6f3e75570f499f4b24a5b73aa45fc83d5ade5de
Rafa Mel <melrcm@gmx.net>
2019-04-15 01:08:43 +0200

    feat: exports tail from entry point

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export { default as flags } from './flags';
 export { default as log } from './log';
 export { default as safePairs } from './safe-pairs';
 export { default as splitBy } from './split-by';
+export { default as tail } from './tail';
 export * from '~/types';
