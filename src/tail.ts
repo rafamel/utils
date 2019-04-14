62febf9bdb8debe0f5ad9078e64df74c42f1baec
Rafa Mel <melrcm@gmx.net>
2019-04-14 15:02:33 +0200

    feat: adds tail

---
+++ src/tail.ts
@@ -0,0 +1,14 @@
+/**
+ * Normalizes suffixed spaces. Given an `arr` array of strings and a number of `spaces`, returns a function that, for a given string, will return that string with the spaces needed to match the number of spaces for the longest string in the array, as a suffix.
+ */
+export default function tail(
+  arr: string[],
+  spaces: number = 8
+): (str: string) => string {
+  const longest = arr.sort((a, b) => b.length - a.length)[0];
+  return function being(str: string): string {
+    return (
+      str + ' '.repeat((longest ? longest.length - str.length : 0) + spaces)
+    );
+  };
+}
