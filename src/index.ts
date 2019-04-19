e3f11fc4bd306816981a7bd76b4688e3b6f5125a
Rafa Mel <melrcm@gmx.net>
2019-04-19 02:30:21 +0200

    feat(tail): removes tail
    BREAKING CHANGE: `tail` is no longer available.

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,4 @@ export { default as loadPackage } from './load-package';
 export { default as log } from './log';
 export { default as safePairs } from './safe-pairs';
 export { default as splitBy } from './split-by';
-export { default as tail } from './tail';
 export * from '~/types';
