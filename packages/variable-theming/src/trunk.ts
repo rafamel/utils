1ba42c54caec00d5c9b056adba158e043c419f6d
Rafa Mel <melrcm@gmx.net>
2019-06-02 09:18:48 +0200

    feat(variable-theming): imlements generate

---
+++ packages/variable-theming/src/trunk.ts
@@ -0,0 +1,44 @@
+import decamelize from 'decamelize';
+import { TItems, IOfType, IOutput } from './types';
+
+export default function trunk(items: TItems, setup: boolean): IOutput {
+  const entries = Object.entries(items);
+  const styles: IOfType<string> = {};
+  let css = ``;
+  const setupStyles: IOfType<IOfType<string>> = {};
+  let setupCss = ``;
+
+  for (let [camelName, element] of entries) {
+    const name = decamelize(camelName, '-');
+    if (setup) {
+      setupStyles[name] = {};
+      setupCss += `\n${name} {`;
+    }
+
+    const entries = Object.entries(element);
+    for (let [camelProperty, value] of entries) {
+      const property = decamelize(camelProperty, '-');
+      const variable = `--${name}-${property}`;
+      styles[variable] = value;
+      css += `\n${variable}: ${value};`;
+
+      if (setup) {
+        const cssvar = `var(${variable});`;
+        setupStyles[name][property] = cssvar;
+        setupCss += `\n  ${property}: ${cssvar}`;
+      }
+    }
+
+    if (setup) {
+      setupCss += '\n}';
+    }
+  }
+
+  return {
+    css: css.trim(),
+    styles,
+    setup: setup
+      ? { css: setupCss.trim(), styles: setupStyles }
+      : { css: '', styles: {} }
+  };
+}
---
+++ packages/variable-theming/src/types.ts
@@ -0,0 +1,14 @@
+export interface IOfType<T> {
+  [key: string]: T;
+}
+
+export type TItems = IOfType<IOfType<string>>;
+
+export interface IOutput {
+  css: string;
+  styles: IOfType<string>;
+  setup: {
+    css: string;
+    styles: IOfType<IOfType<string>>;
+  };
+}
