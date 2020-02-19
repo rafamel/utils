1aa4ba5ce1f6bad4a5cde33882b95be02f5befc0
Rafa Mel <melrcm@gmx.net>
2020-02-19 15:05:09 +0100

    improvement(merge-strategies): improves types

--- packages/merge-strategies/src/index.ts
+++ packages/merge-strategies/src/index.ts
@@ -1,38 +1,43 @@
 import mergewith from 'lodash.mergewith';
 import clonedeep from 'lodash.clonedeep';
 
+export type Merge<D, T> = T & (D extends object ? T & D : T);
+
 /**
  * If both `defaults` and `value` are objects, they will be shallow merged. Mutations to the returned object won't have an effect over `defaults`.
  */
-export function shallow<T, D>(defaults: D, value: T): T | (T & D) {
-  return typeof value === 'object' &&
+export function shallow<D, T = Partial<D>>(defaults: D, value: T): Merge<D, T> {
+  const data: any =
+    typeof value === 'object' &&
     value !== null &&
     !Array.isArray(value) &&
     typeof defaults === 'object' &&
     defaults !== null &&
     !Array.isArray(defaults)
-    ? Object.assign(clonedeep(defaults), value)
-    : value;
+      ? Object.assign(clonedeep(defaults), value)
+      : value;
+  return data;
 }
 
 /**
  * If both `defaults` and `value` are objects, they will be deep merged. Arrays won't be merged. Mutations to the returned object won't have an effect over `defaults`.
  */
-export function merge<T, D>(defaults: D, value: T): T | (T & D) {
-  return mergewith(
+export function merge<D, T = Partial<D>>(defaults: D, value: T): Merge<D, T> {
+  const { data }: any = mergewith(
     {},
     { data: defaults },
     { data: value },
     (obj: any, src: any) =>
       Array.isArray(src) || Array.isArray(obj) ? src : undefined
-  ).data;
+  );
+  return data;
 }
 
 /**
  * If both the `defaults` and `value` are objects, they will be deep merged. Arrays will be concatenated. Mutations to the returned object won't have an effect over `defaults`.
  */
-export function deep<T, D>(defaults: D, value: T): T | (T & D) {
-  return mergewith(
+export function deep<D, T = Partial<D>>(defaults: D, value: T): Merge<D, T> {
+  const { data }: any = mergewith(
     {},
     { data: defaults },
     { data: value },
@@ -42,5 +47,6 @@ export function deep<T, D>(defaults: D, value: T): T | (T & D) {
       if (!a && !b) return;
       return a && b ? obj.concat(src) : src;
     }
-  ).data;
+  );
+  return data;
 }
