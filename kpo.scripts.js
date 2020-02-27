0f0d84834a64a2f76d1b49c827527cd09a6e5a79
Rafa Mel <melrcm@gmx.net>
2020-02-27 08:33:02 +0100

    docs: adds index docs page

--- kpo.scripts.js
+++ kpo.scripts.js
@@ -1,9 +1,11 @@
+const kpo = require('kpo');
 const { scripts } = require('./project.config');
 
 module.exports.scripts = {
   ...scripts,
   bootstrap: 'lerna bootstrap',
   link: 'lerna link',
+  docs: [scripts.docs, kpo.copy('./assets/index.html', './docs/index.html')],
 
   /* Hooks */
   postinstall: 'kpo bootstrap'
