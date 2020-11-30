b603faecc73545e9f289486f6c9e807683a4a30a
Rafa Mel <melrcm@gmx.net>
2020-11-30 19:07:20 +0100

    feat(type-core): adds MaybePromise and Optional types

--- packages/type-core/src/types.ts
+++ packages/type-core/src/types.ts
@@ -16,6 +16,10 @@ export type Members<T = any> = Record<any, T>;
 export type Replace<T extends Members, U> = {
   [P in keyof T]: U;
 };
+export type Optional<T extends Members, K extends keyof T> = Intersection<
+  Omit<T, K>,
+  { [P in K]?: T[P] }
+>;
 
 /* Functions */
 export type NullaryFn<T = void> = () => T;
@@ -23,6 +27,10 @@ export type UnaryFn<T = void, U = void> = (arg: T) => U;
 export type BinaryFn<T extends [any, any], U = void> = (...args: T) => U;
 export type MultiaryFn<T extends any[], U = void> = (...args: T) => U;
 export type VariadicFn<T = any> = (...args: any[]) => T;
+
+/* Promises */
+export type MaybePromise<T> = Union<Promise<T>, T>;
+export type MaybePromiseLike<T> = Union<PromiseLike<T>, T>;
 
 /* Utils */
 export type Union<A, B, C = B, D = B, E = B> = A | B | C | D | E;
